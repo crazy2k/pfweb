648092b82bd05ce441ce804580e91f586010f2cb
Pablo Antonio <pabloa@gmail.com>
2010-01-15 05:37:28 -0300

    Agregar URLs para /static y /pieces
    En static/ iran los archivos que se serviran estaticamente. No seran
    accedidos directamente (por lo que lei de lo que escribio Alberto, eso
    no funciona bien en modo cgi), sino a traves de la url
    <base-URL>/static?file=<file>.
    
    /pieces se usara para devolver fragmentos de HTML, generalmente pedidos
    asincronicamente por funciones de javascript (al estilo AJAX).

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ import pages
 # No tocar NADA salvo esto.
 root = '/pf'
 
+static_dir = 'static/'
+
 urls = (
 	root,				'pages.login',
 	root + '/',			'pages.login',
 	root + '/login',		'pages.login',
-	root + '/style',		'pages.style',
 	root + '/auth',			'pages.auth',
 	root + '/logout',		'pages.logout',
 	root + '/index',		'pages.index',
@@ -25,6 +26,10 @@ urls = (
 	root + '/chpasswd',		'pages.chpasswd',
 	root + '/register',		'pages.register',
 	root + '/mainhelp',		'pages.mainhelp',
+
+	root + '/pieces',		'pages.pieces',
+
+	root + '/static',		'pages.static',
 )
 
 web.internalerror = web.debugerror
