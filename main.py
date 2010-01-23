c0093aabcaa9df9b10d806e0ba4ffea4b37cce2b
Pablo Antonio <pabloa@gmail.com>
2010-01-23 06:22:18 -0300

    Make urls tuple be aligned properly
    I know this doesn't comply with Python's coding style, but I like
    how this looks when it's aligned.

--- main.py
+++ main.py
@@ -11,25 +11,25 @@ root = '/pf'
 static_dir = 'static/'
 
 urls = (
-    root,                'pages.login',
-    root + '/',            'pages.login',
-    root + '/login',        'pages.login',
-    root + '/auth',            'pages.auth',
-    root + '/logout',        'pages.logout',
-    root + '/index',        'pages.index',
-    root + '/setmateria',        'pages.setmateria',
-    root + '/cursandomateria',    'pages.cursandomateria',
-    root + '/corregirnota',        'pages.corregirnota',
-    root + '/datosmateria',        'pages.datosmateria',
+    root,                       'pages.login',
+    root + '/',                 'pages.login',
+    root + '/login',            'pages.login',
+    root + '/auth',             'pages.auth',
+    root + '/logout',           'pages.logout',
+    root + '/index',            'pages.index',
+    root + '/setmateria',       'pages.setmateria',
+    root + '/cursandomateria',  'pages.cursandomateria',
+    root + '/corregirnota',     'pages.corregirnota',
+    root + '/datosmateria',     'pages.datosmateria',
     root + '/listamaterias',    'pages.listamaterias',
-    root + '/personal',        'pages.personal',
-    root + '/chpasswd',        'pages.chpasswd',
-    root + '/register',        'pages.register',
-    root + '/mainhelp',        'pages.mainhelp',
+    root + '/personal',         'pages.personal',
+    root + '/chpasswd',         'pages.chpasswd',
+    root + '/register',         'pages.register',
+    root + '/mainhelp',         'pages.mainhelp',
 
-    root + '/pieces',        'pages.pieces',
+    root + '/pieces',           'pages.pieces',
 
-    root + '/static',        'pages.static',
+    root + '/static',           'pages.static',
 )
 
 web.internalerror = web.debugerror
